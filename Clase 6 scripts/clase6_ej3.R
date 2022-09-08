c5804c54edf5a15678bf887725db78a8b82a7954
tnotenson <83411439+tnotenson@users.noreply.github.com>
2022-09-08 12:27:00 -0300

    clase 6 en Callao
    clase 6

--- Clase 6 scripts/clase6_ej3.R
+++ Clase 6 scripts/clase6_ej3.R
@@ -1,18 +1,20 @@
+# defino parametros
 n = 100
+# estimador de des est
 sqS2 = 12
+# estimador promedio
 Xbar = 64
+# defino alfa
 alfa = 0.05
-
+# grados de libertad
 df = n-1
-
+# cuantiles de la t student
 b = qt(1-alfa/2, df)
 a = qt(alfa/2, df)
-
 print(a)
 print(b)
-
+# intervalo de confianza
 IC_l = Xbar - b * sqS2/sqrt(n)
 IC_u = Xbar - a * sqS2/sqrt(n)
-
 print(IC_l)
 print(IC_u)
